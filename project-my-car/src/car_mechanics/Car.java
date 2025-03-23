7aa9d22ce8533b3d976d67de06fd279c66c7f19e
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-03-22 21:18:14 -0400

    feat: implementar o método de desligar o carro.

--- project-my-car/src/car_mechanics/Car.java
+++ project-my-car/src/car_mechanics/Car.java
@@ -10,14 +10,27 @@ public class Car {
 		this.speed = 0;
 		this.gear = 0;
 	}
-	
+
 	// Método para ligar o carro
 	public void turnOn() {
 		if (!carIsOn) {
 			carIsOn = true;
 			System.out.println("O carro foi ligado.");
-		}else {
+		} else {
 			System.out.println("O carro foi desligado.");
+		}
+	}
+
+	// Método para desligar o carro
+	public void turnOff() {
+		if (carIsOn && gear == 0 && speed == 0) {
+			carIsOn = false;
+			System.out.println("O carro foi desligado.");
+		} else if (gear != 0 || speed != 0) {
+			System.out.println("Não é possível desligar o carro. "
+					+ "Ele deve estar em ponto morto (marcha 0) e com velocidade 0 km/h.");
+		} else {
+			System.out.println("O carro já está desligado.");
 		}
 	}
 }
