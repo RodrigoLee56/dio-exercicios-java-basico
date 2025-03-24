6bda284fde31e2aff65f6eb86db306b203bf87e7
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-03-23 23:26:25 -0400

    feat: implementar o metodo main

--- project-my-car/src/app/MainCar.java
+++ project-my-car/src/app/MainCar.java
@@ -5,17 +5,46 @@ import java.util.Scanner;
 import car_mechanics.Car;
 
 public class MainCar {
+	private static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
 		Car car = new Car();
 
 		while (true) {
 			menu();
-			int option = scan.nextInt();
+
+			int option = readIntInput("Escolha uma opção: ", 1, 8);
+
 			switch (option) {
 			case 1:
 				car.turnOn();
+				break;
+			case 2:
+				car.turnOff();
+				break;
+			case 3:
+				car.accelerate();
+				break;
+			case 4:
+				car.decreaseSpeed();
+				break;
+			case 5:
+				car.checkSpeed();
+				break;
+			case 6:
+				int newGear = readIntInput("Informe a nova marcha (0-6): ", 0, 6);
+				car.changeGear(newGear);
+				break;
+			case 7:
+				String side = readStringInput("Para qual lado deseja virar? (esquerda/direita): ");
+				car.turn(side);
+				break;
+			case 8:
+				System.out.println("Encerrando o programa...");
+				scan.close();
+				return;
+			default:
+				System.out.println("Opção inválida. Tente novamente.");
 				break;
 			}
 		}
@@ -31,8 +60,28 @@ public class MainCar {
 		System.out.println("6. Trocar marcha");
 		System.out.println("7. Virar (esquerda/direita)");
 		System.out.println("8. Sair");
-		System.out.print("Escolha uma opção: ");
-
+		System.out.print("");
 	}
 
+	private static int readIntInput(String message, int min, int max) {
+		while (true) {
+			try {
+
+				System.out.println(message);
+				int input = Integer.parseInt(scan.nextLine());
+				if (input >= min && input <= max) {
+					return input;
+				} else {
+					System.out.println("Valor fora do intervalo permitido. Tente novamente.");
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("Entrada inválida. Insira um número inteiro.");
+			}
+		}
+	}
+
+	private static String readStringInput(String message) {
+		System.out.print(message);
+		return scan.nextLine().trim();
+	}
 }
